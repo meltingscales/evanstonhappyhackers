e18c9eda27a4df925362244e589e1855be40dc30
Henry Post <HenryFBP@gmail.com>
2024-12-07 05:31:49 -0600

    tic tac toe stub

---
+++ 2024-12-05-TicTacToeWithAndy/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+int main() {
+
+
+    int* myTicTacToeBoard = new int[9];
+
+    // 012
+    // 345
+    // 678
+
+    // i want a board that looks like this:
+    // XXO
+    // XOX
+    // OOO
+
+    // XXO XOX OOO
+    //X = one
+    //O = zero
+
+    // first row
+    myTicTacToeBoard[0] = 1; // top left,   x/1
+    myTicTacToeBoard[1] = 1; // top middle, x/1
+    myTicTacToeBoard[2] = 0; // top right,  o/0
+
+    //second row
+    myTicTacToeBoard[3] = 0; //left,
+    myTicTacToeBoard[4] = 0;
+    myTicTacToeBoard[5] = 0;
+
+    //third row
+    myTicTacToeBoard[6] = 0;
+    myTicTacToeBoard[7] = 0;
+    myTicTacToeBoard[8] = 0;
+
+    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
+    // <b>lang</b> variable name to see how CLion can help you rename it.
+    auto lang = "C++";
+    std::cout << "Hello and welcome to " << lang << "!\n";
+
+    for (int i = 1; i <= 5; i++) {
+        // TIP Press <shortcut actionId="Debug"/> to start debugging your code.
+        // We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/>
+        // breakpoint for you, but you can always add more by pressing
+        // <shortcut actionId="ToggleLineBreakpoint"/>.
+        std::cout << "i = " << i << std::endl;
+    }
+
+    return 0;
+}
+
+// TIP See CLion help at <a
+// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
+//  Also, you can try interactive lessons for CLion by selecting
+//  'Help | Learn IDE Features' from the main menu.
